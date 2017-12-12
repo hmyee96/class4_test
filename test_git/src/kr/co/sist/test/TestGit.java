604755beb774c3dc5cd056507dfdd7ab5bd5c4f8
sist <sist@sist-PC>
2017-12-12 12:45:29 +0900

    고친거

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -5,7 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		int i=10;
 		System.out.println("Git Hub 연습 "+i);
+		System.out.println("수정수정수정");
+	}//end main
 
-	}
-
-}
+}//class
